dd40066b414a30f3b66fc4955557a46bf5c15b78
KaranVikas <karanvikas15@gmail.com>
2025-08-30 16:56:59 -0400

    Add test coverage for UserOrder endpoint
    - Implemented `UserOrderTestCase` with authenticated and unauthenticated access scenarios.
    - Enforced `IsAuthenticated` permission for `UserOrderListAPIView`.
    - Updated URL patterns to include a named route for `user-orders`.

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 
 
@@ -25,6 +26,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
   queryset = Order.objects.prefetch_related('items__product')
   serializer_class = OrderSerializer
+  permission_classes = [IsAuthenticated]
 
   def get_queryset(self):
     user = self.request.user
