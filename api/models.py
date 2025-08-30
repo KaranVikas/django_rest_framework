f612fb83734242eedce408dcffa4d981bb2443dd
KaranVikas <karanvikas15@gmail.com>
2025-08-30 11:58:25 -0400

    Initialize Django project with essential setup and models.
    - Added `.idea` configuration to `.gitignore`.
    - Created Django app `api` with `User`, `Product`, `Order`, and `OrderItem` models.
    - Created initial migration for `api` app.
    - Implemented database population management command (`populate_db.py`).
    - Updated related project settings and helper files (e.g., `manage.py`, `asgi.py`).

---
+++ api/models.py
@@ -0,0 +1,55 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+  pass
+
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.TextField(max_length=500)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  stock = models.PositiveIntegerField()
+  image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+  @property
+  def in_stock(self):
+    return self.stock > 0
+
+  def __str__(self):
+    return self.name
+
+class Order(models.Model):
+  class StatusChoices(models.TextChoices):
+    PENDING = 'Pending'
+    CANCELLED = 'Cancelled'
+    CONFIRMED = 'Confirmed'
+
+  order_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  # if user model , delete - remove all the orders from the system
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  status = models.CharField(max_length=10, choices=StatusChoices.choices, default= StatusChoices.PENDING)
+
+  products = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+
+  def __str__(self):
+    return f"Order {self.order_id} for {self.user.username} - {self.status}"
+
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete= models.CASCADE)
+  quantity = models.PositiveIntegerField()
+
+  @property
+  def item_subtotal(self):
+    return self.product.price * self.quantity
+
+  def __str__(self):
+    return f"{self.quantity} * {self.product.name} in Order {self.order.order_id}"
+
+
+
+
+
---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def product_list(request):
+  # fetch all the products
+  products = Product.objects.all()
+  # create a serialiser ,many=True bcx many objects
+  serializer = ProductSerializer(products, many=True)
+  return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+  # fetch all the products
+  product = get_object_or_404(Product, pk=pk)
+  # create a serialiser ,many=True bcx many objects
+  serializer = ProductSerializer(product)
+  return Response(serializer.data)
