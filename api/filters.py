a5884e395181a066d6650b7e2497c284246b7e05
KaranVikas <karanvikas15@gmail.com>
2025-08-31 01:12:38 -0400

    Search filter, Ordering Filter and Customizable filter
    - Enhanced product filtering by adding `InStockFilterBackend` to exclude products with stock greater than 0.
    
    - Updated `ProductFilter` to support case-insensitive exact lookups for `name`.
    - Added ordering, search, and filter capabilities to `ProductListCreateAPIView`.

--- api/filters.py
+++ api/filters.py
@@ -1,12 +1,16 @@
 import django_filters
-
+from rest_framework import filters
 from api.models import Product
 
+class InStockFilterBackend(filters.BaseFilterBackend):
+  def filter_queryset(self, request, queryset, view):
+    return queryset.exclude(stock__gt=0)
 
 class ProductFilter(django_filters.FilterSet):
   class Meta:
     model = Product
     fields = {
-      'name': ['exact', 'contains'],
+      'name': ['iexact', 'contains'],
       'price': ['exact','lt','gt','range'],
     }
+
--- api/views.py
+++ api/views.py
@@ -7,13 +7,24 @@ from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser , AllowAny
 from rest_framework.views import APIView
-from api.filters import ProductFilter
+from api.filters import ProductFilter, InStockFilterBackend
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create a class based view
 class ProductListCreateAPIView(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   filterset_class = ProductFilter
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter, InStockFilterBackend]
+  ordering_fields = ['name', 'price', 'stock']
+  ordering = ['name']
+  filterset_fields = [
+    'name',
+    'price',
+    'stock',
+  ]
+  search_fields = ['=name', 'description']
 
   def get_permissions(self):
     self.permission_classes = [AllowAny]
