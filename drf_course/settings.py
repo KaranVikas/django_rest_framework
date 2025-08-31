1ac191a28f8f21ca37f2c7afa5779da45918ef1f
KaranVikas <karanvikas15@gmail.com>
2025-08-30 23:36:01 -0400

    Integrated `drf-spectacular` for OpenAPI schema generation
    - Added `drf_spectacular` to installed apps and `DEFAULT_SCHEMA_CLASS` in REST framework settings.
    - Configured `SPECTACULAR_SETTINGS` for title, description, and version of the API.
    - Introduced schema and UI endpoints (`api/schema/`, `api/schema/swagger-ui/`, and `api/schema/redoc/`).
    - Updated `requirements.txt` to include `drf-spectacular` and its dependencies.

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'api',
     'rest_framework',
-    'silk'
+    'silk',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +122,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'E-commerce App',
+    'DESCRIPTION': 'A simple product & order api that helps us learn django',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
