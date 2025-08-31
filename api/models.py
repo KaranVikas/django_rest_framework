255bb6535edccfdb44cc57fe8fdaa85f297c78ab
KaranVikas <karanvikas15@gmail.com>
2025-08-31 00:34:20 -0400

    django-filter FilterSetBackend
    - Integrated `django-filter` into the project and updated `requirements.txt`.
    - Configured `DEFAULT_FILTER_BACKENDS` in REST framework settings.
    - Created `ProductFilter` in `api/filters.py` to enable filtering by name and price.
    - Applied `ProductFilter` to `ProductListCreateAPIView`.

--- api/models.py
+++ api/models.py
@@ -31,7 +31,6 @@ class Order(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   status = models.CharField(max_length=10, choices=StatusChoices.choices, default= StatusChoices.PENDING)
-
   products = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
 
   def __str__(self):
